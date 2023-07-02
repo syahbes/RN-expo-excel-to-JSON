de98d2b2da57b59dbfdbf6ed926e22ea95e0a6e8
syahbes <syahbes@gmail.com>
2023-07-02 21:39:14 +0300

    working

--- ExcelParser.js
+++ ExcelParser.js
@@ -1,23 +1,23 @@
 import * as xlsx from 'xlsx';
+import base64js from 'base64-js';
 
 export default async function parseExcelToJson(fileUrl) {
-    const res = await fetch(fileUrl);
-    const ab = await res.arrayBuffer(); // recover data as ArrayBuffer
-    const wb = xlsx.read(ab);
-    const firstSheet = wb.SheetNames[0];
-    const worksheet = wb.Sheets[firstSheet];
-    const jsonData = xlsx.utils.sheet_to_json(worksheet, { header: 1, raw: false });
-    const headerRow = jsonData.shift(); // Remove the header row from the data
-
-    const cleanedData = jsonData.map(row => {
-      const cleanedRow = {};
-      row.forEach((value, index) => {
-        const header = headerRow[index]?.trim(); // Remove leading and trailing spaces
-        const cleanedValue = value?.trim(); // Remove leading and trailing spaces from value
-        cleanedRow[header] = cleanedValue;
+  const res = await fetch(fileUrl);
+  const blob = await res.blob(); // recover data as Blob
+  const reader = new FileReader();
+  reader.readAsDataURL(blob);
+  return new Promise((resolve, reject) => {
+    reader.onloadend = function () {
+      const binaryString = base64js.toByteArray(reader.result.split(',')[1]);
+      const workbook = xlsx.read(binaryString, { type: 'array' });
+      const sheetNameList = workbook.SheetNames;
+      const sheetData = sheetNameList.map((sheetName) => {
+        return xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
       });
-      return cleanedRow;
-    });
-
-    return cleanedData;
+      resolve(sheetData);
+    };
+    reader.onerror = function () {
+      reject(new Error('Failed to read file'));
+    };
+  });
 }
